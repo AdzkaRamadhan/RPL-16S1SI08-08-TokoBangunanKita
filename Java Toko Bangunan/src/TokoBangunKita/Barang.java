48df9f824b2b69461514faf9bfb0dcf4fc85214d
Emma Paris <eparis@atlassian.com>
2018-11-23 16:54:21 +0700

    folder java

---
+++ Java Toko Bangunan/src/TokoBangunKita/Barang.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TokoBangunKita;
+
+/**
+ *
+ * @author acer
+ */
+public class Barang {
+    private String kdBarang;
+    private String namaBarang;
+    private int stokBarang;
+    private int hargaJual;
+    private int hargaBeli;
+    /**
+     * @return the kdBarang
+     */
+    public String getKdBarang() {
+        return kdBarang;
+    }
+
+    /**
+     * @param kdBarang the kdBarang to set
+     */
+    public void setKdBarang(String kdBarang) {
+        this.kdBarang = kdBarang;
+    }
+
+    /**
+     * @return the namaBarang
+     */
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    /**
+     * @param namaBarang the namaBarang to set
+     */
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    /**
+     * @return the stokBarang
+     */
+    public int getStokBarang() {
+        return stokBarang;
+    }
+
+    /**
+     * @param stokBarang the stokBarang to set
+     */
+    public void setStokBarang(int stokBarang) {
+        this.stokBarang = stokBarang;
+    }
+
+    /**
+     * @return the hargaJual
+     */
+    public int getHargaJual() {
+        return hargaJual;
+    }
+
+    /**
+     * @param hargaJual the hargaJual to set
+     */
+    public void setHargaJual(int hargaJual) {
+        this.hargaJual = hargaJual;
+    }
+
+    /**
+     * @return the hargaBeli
+     */
+    public int getHargaBeli() {
+        return hargaBeli;
+    }
+
+    /**
+     * @param hargaBeli the hargaBeli to set
+     */
+    public void setHargaBeli(int hargaBeli) {
+        this.hargaBeli = hargaBeli;
+    }
+    
+}
---
+++ Java Toko Bangunan/src/TokoBangunKita/Pelanggan.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TokoBangunKita;
+
+/**
+ *
+ * @author acer
+ */
+public class Pelanggan {
+    private String kdPelanggan;
+    private String  namaPelanggan;
+    private int noTelponPelanggan;
+    private String alamatPelanggan;
+    /**
+     * @return the kdPelanggan
+     */
+    public String getKdPelanggan() {
+        return kdPelanggan;
+    }
+
+    /**
+     * @param kdPelanggan the kdPelanggan to set
+     */
+    public void setKdPelanggan(String kdPelanggan) {
+        this.kdPelanggan = kdPelanggan;
+    }
+
+    /**
+     * @return the namaPelanggan
+     */
+    public String getNamaPelanggan() {
+        return namaPelanggan;
+    }
+
+    /**
+     * @param namaPelanggan the namaPelanggan to set
+     */
+    public void setNamaPelanggan(String namaPelanggan) {
+        this.namaPelanggan = namaPelanggan;
+    }
+
+    /**
+     * @return the noTelponPelanggan
+     */
+    public int getNoTelponPelanggan() {
+        return noTelponPelanggan;
+    }
+
+    /**
+     * @param noTelponPelanggan the noTelponPelanggan to set
+     */
+    public void setNoTelponPelanggan(int noTelponPelanggan) {
+        this.noTelponPelanggan = noTelponPelanggan;
+    }
+
+    /**
+     * @return the alamatPelanggan
+     */
+    public String getAlamatPelanggan() {
+        return alamatPelanggan;
+    }
+
+    /**
+     * @param alamatPelanggan the alamatPelanggan to set
+     */
+    public void setAlamatPelanggan(String alamatPelanggan) {
+        this.alamatPelanggan = alamatPelanggan;
+    }
+}
